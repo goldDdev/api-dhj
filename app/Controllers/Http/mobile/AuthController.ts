c66746c7ff05f7624191213ed93a8f5a972d45e9
ary21 <aryjamers.21@gmail.com>
2023-03-12 14:30:59 +0700

    mobile auth

--- app/Controllers/Http/mobile/AuthController.ts
+++ app/Controllers/Http/mobile/AuthController.ts
@@ -1,4 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Employee from 'App/Models/Employee'
+import User from 'App/Models/User'
 
 export default class AuthController {
   public async login({ auth, request, response }: HttpContextContract) {
@@ -8,11 +12,104 @@ export default class AuthController {
       return response.send({
         data: {
           token,
-          user: auth.user?.serialize(),
+          // user: auth.user?.serialize(),
         },
       })
     } catch {
       return response.badRequest({ error: 'Invalid credentials' })
+    }
+  }
+
+  public async logout({ auth, request, response }: HttpContextContract) {
+    try {
+      await auth.use('api').revoke()
+      return response.send(204)
+    } catch {
+      return response.badRequest({ error: 'Invalid credentials' })
+    }
+  }
+
+  public async current({ auth, request, response }: HttpContextContract) {
+    try {
+      await auth.use('api').authenticate()
+      const user = await User.query()
+        .where('id', auth.use('api').user!.id)
+        .preload('employee')
+        .firstOrFail()
+      return response.send({ user })
+    } catch (error) {
+      return response.unprocessableEntity({ error })
+    }
+  }
+
+  public async editProfile({ auth, request, response }: HttpContextContract) {
+    const trx = await Database.transaction()
+    try {
+      await auth.use('api').authenticate()
+      const currentUser = auth.use('api').user!
+      const payload = await request.validate({
+        schema: schema.create({
+          name: schema.string([rules.minLength(3)]),
+          phoneNumber: schema.string([
+            rules.minLength(10),
+            rules.unique({
+              table: 'employees',
+              column: 'phone_number',
+              whereNot: {
+                id: currentUser.id,
+              },
+            }),
+          ]),
+          email: schema.string.optional([
+            rules.unique({
+              table: 'users',
+              column: 'email',
+              whereNot: {
+                email: null,
+                id: currentUser.id,
+              },
+            }),
+          ]),
+        }),
+      })
+
+      const { email, ...emp } = payload
+      const model = await Employee.find(currentUser.employeeId, { client: trx })
+      if (model) {
+        await model.load('user')
+        await model.merge(emp).save()
+        if (email) {
+          await model.user.merge({ email }).save()
+        }
+        await trx.commit()
+        return response.status(204)
+      }
+    } catch (error) {
+      console.log(error)
+      await trx.rollback()
+      return response.unprocessableEntity({ error })
+    }
+  }
+
+  public async changePassword({ auth, request, response }: HttpContextContract) {
+    try {
+      await auth.use('api').authenticate()
+      const currentUser = auth.use('api').user!
+      console.log('cel')
+      await request.validate({
+        schema: schema.create({
+          password: schema.string([rules.minLength(3)]),
+        }),
+      })
+
+      const user = await User.findOrFail(currentUser.id)
+      // TODO : current password validation
+      const { currentPassword, password } = request.body()
+      await user.merge({ password }).save()
+      // TODO : send to user.email
+      return response.status(204)
+    } catch (error) {
+      return response.unprocessableEntity({ error })
     }
   }
 }
--- start/mobile.ts
+++ start/mobile.ts
@@ -3,6 +3,10 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.group(() => {
     Route.post('/login', 'AuthController.login')
+    Route.post('/logout', 'AuthController.logout')
+    Route.get('/current', 'AuthController.current')
+    Route.put('/edit-profile', 'AuthController.editProfile')
+    Route.put('/change-password', 'AuthController.changePassword')
   }).prefix('auth')
 })
   .namespace('App/Controllers/Http/mobile')

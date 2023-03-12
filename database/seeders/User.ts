f534d8c711820ec70289398bb1bc87768dec7df1
ary21 <aryjamers.21@gmail.com>
2023-03-12 14:37:16 +0700

    emp seed

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,12 +1,28 @@
-import User from 'App/Models/User'
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+import Employee from 'App/Models/Employee'
 
 export default class extends BaseSeeder {
   public async run() {
+    await Employee.createMany([
+      {
+        id: 1,
+        name: 'Budi',
+        cardID: '123412341234',
+        phoneNumber: '089123123123',
+        role: 'SPV',
+      },
+    ])
+
     await User.createMany([
       {
         email: 'test@example.com',
         password: 'P@ssw0rd',
+      },
+      {
+        email: 'spv@example.com',
+        password: 'P@ssw0rd',
+        employeeId: 1,
       },
     ])
   }

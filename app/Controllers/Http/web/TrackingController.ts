fb112cc32b13345c0faebe67178e46ec8c4fd2c8
ary21 <aryjamers.21@gmail.com>
2023-04-01 23:51:05 +0700

    tracking center

--- app/Controllers/Http/web/TrackingController.ts
+++ app/Controllers/Http/web/TrackingController.ts
@@ -1,10 +1,13 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
+import Project from 'App/Models/Project'
 import Tracking from 'App/Models/Tracking'
 
 export default class TrackingsController {
   public async index({ request, response }: HttpContextContract) {
     if (!request.input('projectId') || !request.input('date')) return response.send({ data: [] })
+    const project = await Project.findOrFail(request.input('projectId'))
+
     const tracks = await Tracking.query()
       .where('projectId', request.input('projectId'))
       .where(Database.raw('DATE(created_at)'), request.input('date'))
@@ -14,7 +17,7 @@ export default class TrackingsController {
 
     return response.send({
       data: tracks,
-      meta: { center: { latitude: '1.598333', longitude: '101.431827' } },
+      meta: { center: { latitude: project.latitude, longitude: project.longitude } },
     })
   }
 }

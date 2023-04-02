aa6ba3d13b7ef14c9da99d267cef4f18ba6eaa55
ary21 <aryjamers.21@gmail.com>
2023-04-02 09:56:45 +0700

    fix tracking

--- app/Controllers/Http/web/TrackingController.ts
+++ app/Controllers/Http/web/TrackingController.ts
@@ -1,19 +1,32 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Project from 'App/Models/Project'
-import Tracking from 'App/Models/Tracking'
+// import Tracking from 'App/Models/Tracking'
 
 export default class TrackingsController {
   public async index({ request, response }: HttpContextContract) {
     if (!request.input('projectId') || !request.input('date')) return response.send({ data: [] })
     const project = await Project.findOrFail(request.input('projectId'))
 
-    const tracks = await Tracking.query()
-      .where('projectId', request.input('projectId'))
-      .where(Database.raw('DATE(created_at)'), request.input('date'))
-    // TODO : need preload join project & employee
+    const tracks = await Database.from('trackings')
+      .select(
+        'project_id',
+        'projects.latitude AS project_latitude',
+        'projects.longitude AS project_longitude',
+        'employee_id',
+        'employees.name',
+        'employees.role',
+        'trackings.latitude',
+        'trackings.longitude'
+      )
+      .joinRaw('JOIN projects ON projects.id = trackings.project_id')
+      .joinRaw('JOIN employees ON employees.id = trackings.employee_id')
+      .where('trackings.project_id', request.input('projectId'))
+      .where(Database.raw('DATE(trackings.created_at)'), request.input('date'))
+      .orderBy('trackings.created_at', 'desc')
+    // .groupBy('employeeId')
 
-    // TODO : need send center location => should be coordinate project
+    // TODO : employee project but not in trackings ?
 
     return response.send({
       data: tracks,

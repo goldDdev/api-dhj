1dfe5aef2b18dd0e02bd664c4f030f0100a40ca1
ary21 <aryjamers.21@gmail.com>
2023-05-14 07:15:23 +0700

    cek track

--- app/Controllers/Http/web/TrackingController.ts
+++ app/Controllers/Http/web/TrackingController.ts
@@ -6,32 +6,34 @@ export default class TrackingsController {
   public async index({ request, response, now }: HttpContextContract) {
     if (!request.input('projectId') || !request.input('date')) return response.send({ data: [] })
     const project = await Project.findOrFail(request.input('projectId'))
+    console.log('input >', request.input('date'))
     console.log('project >', project)
 
     const tracks = (
       await Database.rawQuery(
         `SELECT 
-        DISTINCT ON(employee_id)
-        tr.id,
-        project_id,
-        projects.latitude AS project_latitude,
-        projects.longitude AS project_longitude,
-        employee_id,
-        employees.name,
-        employees.role,
-        tr.latitude,
-        tr.longitude,
-        tr.created_at
-      FROM
-        (SELECT * FROM trackings ORDER BY id DESC) as tr
-      JOIN projects ON projects.id = tr.project_id
-      JOIN employees ON employees.id = tr.employee_id
-      WHERE DATE(tr.created_at) = :date
+          DISTINCT ON(employee_id)
+          tr.id,
+          project_id,
+          projects.latitude AS project_latitude,
+          projects.longitude AS project_longitude,
+          employee_id,
+          employees.name,
+          employees.role,
+          tr.latitude,
+          tr.longitude,
+          tr.created_at
+        FROM
+          (SELECT * FROM trackings ORDER BY id DESC) as tr
+        JOIN projects ON projects.id = tr.project_id
+        JOIN employees ON employees.id = tr.employee_id
+        WHERE DATE(tr.created_at) = :date
       `,
         { date: request.input('date', now) }
       )
     ).rows
     console.log('tracks >', tracks)
+    console.log('cek >', (await Database.rawQuery('SELECT * FROM trackings ORDER BY id DESC')).rows)
 
     // TODO : employee project but not in trackings ?
 
